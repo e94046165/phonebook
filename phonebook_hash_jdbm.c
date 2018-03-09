11e45e5da559b6090055833b4d7e6e26558f5a65
e94046165 <e94046165@mail.ncku.edu.tw>
2018-03-09 21:10:13 +0800

    Use the hash function jdbm
    To compare the performance of hash functions.
    I try another hash function.

---
+++ phonebook_hash_jdbm.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+
+#include "phonebook_hash_jdbm.h"
+
+/* TODO: FILL YOUR OWN IMPLEMENTATION HERE! */
+unsigned long hash(unsigned char *str)
+{
+    unsigned long hash = 0;
+    int c;
+
+    while (c = *str++)
+        hash = c + (hash << 6) + (hash << 16) - hash;
+
+    return hash;
+}
+
+entry *findName(char lastName[], entry **pHead)
+{
+    //printf("get into find func\n");
+    entry *e = pHead[hash(lastName)%HASH_TABLE_SIZE];
+    while (e != NULL) {
+        if (strcasecmp(lastName, e->lastName) == 0) {
+            //printf("I find :%s!!\n", e->lastName) ;
+            return e;
+        }
+        //printf("not the word : %s\n", e->lastName);
+        e = e->pNext;
+    }
+    return NULL;
+}
+
+entry *append(char lastName[], entry **pHead)
+{
+    //printf("get into append func\n");
+    /* allocate memory for the new entry and put lastName */
+    unsigned long index = hash(lastName)%HASH_TABLE_SIZE;
+    entry *e = (entry *) malloc(sizeof(entry));
+    e->pNext = pHead[index]->pNext;
+    pHead[index]->pNext = e;
+    strcpy(e->lastName, lastName);
+
+    return e;
+}
---
+++ phonebook_hash_jdbm.h
@@ -0,0 +1,33 @@
+#ifndef _PHONEBOOK_H
+#define _PHONEBOOK_H
+
+#define MAX_LAST_NAME_SIZE 16
+#define HASH_TABLE_SIZE 1024
+
+
+/* TODO: After modifying the original version, uncomment the following
+ * line to set OPT properly */
+#define OPT 1
+#define HASH2 1
+
+typedef struct __attribute__((packed)) __PHONE_BOOK_ENTRY {
+    char lastName[MAX_LAST_NAME_SIZE];
+    struct __PHONE_BOOK_DETAIL *additional_details;
+    struct __PHONE_BOOK_ENTRY *pNext;
+} entry;
+typedef struct __PHONE_BOOK_DETAIL {
+    char firstName[16];
+    char email[16];
+    char phone[10];
+    char cell[10];
+    char addr1[16];
+    char addr2[16];
+    char city[16];
+    char state[2];
+    char zip[5];
+} details;
+
+entry *findName(char lastName[], entry **pHead);
+entry *append(char lastName[], entry **pHead);
+
+#endif
